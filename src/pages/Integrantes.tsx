e0bb59a777bc23a45f0265865e114212e44b5f72
Angello Turano <73845257+IHateMyTiming@users.noreply.github.com>
2024-09-16 17:03:21 -0300

    arrumando a pagina integrantes
    o module.css estava dando errado, as imagens estavam umas em cima das outras e nao umas aos lados das outras

--- src/pages/Integrantes.tsx
+++ src/pages/Integrantes.tsx
@@ -8,26 +8,26 @@ const Integrantes: React.FC = () => {
   return (
     <div className={styles.integrantesContainer}>
       <div className={styles.integrante}>
-        <img src={integrante1} alt="Integrante 1" />
-        <p>Angello Turano da Costa</p>
-        <p>RM556511</p>
-        <a href="https://github.com/AngelloTDC" target="_blank" rel="noopener noreferrer">
+        <img src={integrante1} alt="Integrante 1" className={styles.integranteImage} />
+        <p className={styles.integranteText}>Angello Turano da Costa</p>
+        <p className={styles.integranteText}>RM556511</p>
+        <a href="https://github.com/AngelloTDC" target="_blank" rel="noopener noreferrer" className={styles.integranteLink}>
           Meu GitHub
         </a>
       </div>
       <div className={styles.integrante}>
-        <img src={integrante2} alt="Integrante 2" />
-        <p>Leonardo Kawachi</p>
-        <p>RM558166</p>
-        <a href="https://github.com/IHateMyTiming" target="_blank" rel="noopener noreferrer">
+        <img src={integrante2} alt="Integrante 2" className={styles.integranteImage} />
+        <p className={styles.integranteText}>Leonardo Kawachi</p>
+        <p className={styles.integranteText}>RM558166</p>
+        <a href="https://github.com/IHateMyTiming" target="_blank" rel="noopener noreferrer" className={styles.integranteLink}>
           Meu GitHub
         </a>
       </div>
       <div className={styles.integrante}>
-        <img src={integrante3} alt="Integrante 3" />
-        <p>Victor Nieves Britto Medeiros</p>
-        <p>RM554557</p>
-        <a href="https://github.com/victornbm7405" target="_blank" rel="noopener noreferrer">
+        <img src={integrante3} alt="Integrante 3" className={styles.integranteImage} />
+        <p className={styles.integranteText}>Victor Nieves Britto Medeiros</p>
+        <p className={styles.integranteText}>RM554557</p>
+        <a href="https://github.com/victornbm7405" target="_blank" rel="noopener noreferrer" className={styles.integranteLink}>
           Meu GitHub
         </a>
       </div>
@@ -36,4 +36,5 @@ const Integrantes: React.FC = () => {
 };
 
 export default Integrantes;
+
 
